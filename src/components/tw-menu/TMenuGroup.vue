1f0858bcc41c81e818a6b49311f23bd9ea9b04ec
dywedzfc <dywedzfc@163.com>
2019-07-05 09:51:27 +0800

    菜单栏

--- src/components/tw-menu/TMenuGroup.vue
+++ src/components/tw-menu/TMenuGroup.vue
@@ -2,35 +2,27 @@
   <div class="tw-menu-group">
     <div
       class="tw-menu-title"
-      :class="{
-        active: hasActiveMenuPanel(data),
-        open: hasOpenMenuPanel(data)
-      }"
+      :class="{active: hasActiveMenuPanel, open: hasOpenMenuPanel}"
       @click="handlerMenuTitleClick(data)"
     >
       <t-icon :icon="data.icon"></t-icon>
-      {{ data.title }}
-      <t-icon class="tw-suffix-icon" icon="el-icon-arrow-down"></t-icon>
+      <span class="tw-title" v-text="data.title"></span>
+      <t-icon class="tw-suffix-icon" :icon="iconClassName('icon-expansion-d')"></t-icon>
     </div>
-    <div class="tw-menu-panel" :class="{ open: hasOpenMenuPanel(data) }">
-      <template v-for="item in data.children">
-        <t-w-menu-group
-          :key="item.id"
-          :data="item"
-          :active.sync="active"
-          :open="openPanelItem"
-          @change-open="changeOpenPanel"
-          v-if="item.children"
-        ></t-w-menu-group>
-        <t-menu-item
-          :key="item.id"
-          :data="item"
-          :active.sync="active"
-          @click.native="handlerMenuItemClick"
-          v-else
-        ></t-menu-item>
-      </template>
-    </div>
+    <transition name="menu-panel-fade">
+      <div class="tw-menu-panel" :class="{ open: hasOpenMenuPanel }">
+        <template v-for="item in data.children">
+          <t-menu-group
+            :key="item.id"
+            :data="item"
+            :open="openPanelItem"
+            @change-open="changeOpenPanel"
+            v-if="item.children"
+          ></t-menu-group>
+          <t-menu-item :key="item.id" :data="item" @click.native="handlerMenuItemClick" v-else></t-menu-item>
+        </template>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -38,6 +30,7 @@
 import _ from 'underscore'
 import TMenuItem from './TMenuItem'
 import TIcon from '../TIcon'
+import { iconClassName } from 'assets/js/util'
 
 export default {
   name: 'TMenuGroup',
@@ -48,7 +41,6 @@ export default {
   },
   props: {
     data: Object,
-    active: String,
     open: Object
   },
   mounted() {
@@ -65,46 +57,42 @@ export default {
     currentOpenPanel() {
       const menuList = this.data
       return _.filter(menuList.children, this.filterMenuChildren)[0] || {}
+    },
+    hasActiveMenuPanel() {
+      const item = this.data
+      const routerPath = this.currentPath
+      return routerPath.indexOf(item.fullPath) === 0
+    },
+    hasOpenMenuPanel() {
+      const item = this.data
+      const path = this.currentPath
+      const { fullPath } = this.open
+      const activePath = path.indexOf(item.fullPath) === 0
+      if (fullPath === item.fullPath && activePath) return true
+      else if (fullPath === item.fullPath) return true
+      else if (!fullPath && activePath) return true
+      else return false
     }
   },
   methods: {
+    iconClassName,
     changeOpenPanel(item) {
       this.openPanelItem = item
-      console.info('TWMenuGroup-changeOpenPanel:', item)
     },
     handlerMenuItemClick() {
       this.openPanelItem = {}
     },
     handlerMenuTitleClick(item) {
-      console.info(
-        'TWMenuGroup-handlerMenuTitleClick:',
-        item,
-        this.open.id,
-        this.open.fullPath,
-        item.fullPath
-      )
-      if (this.open.id && this.open.fullPath === item.fullPath)
-        this.$emit('change-open', {})
-      else
-        this.$emit('change-open', {
-          id: item.id,
-          title: item.title,
-          fullPath: item.fullPath
-        })
-    },
-    hasActiveMenuPanel(item) {
-      const routerPath = this.currentPath
-      return routerPath.indexOf(item.fullPath) === 0
-    },
-    hasOpenMenuPanel(item) {
-      const active = this.active
-      const openPanelItem = this.open
-      return openPanelItem
-        ? openPanelItem.fullPath === item.fullPath
-        : active.indexOf(item.fullPath) === 0
+      // const path = this.currentPath
+      const { fullPath } = this.open
+      if (fullPath === item.fullPath) this.$emit('change-open', {})
+      else this.$emit('change-open', this.resetMenuItem(item))
     },
     filterMenuChildren(item) {
       return item.fullPath && this.currentPath.indexOf(item.fullPath) === 0
+    },
+    resetMenuItem(item) {
+      return { id: item.id, title: item.title, fullPath: item.fullPath }
     }
   },
   components: {
